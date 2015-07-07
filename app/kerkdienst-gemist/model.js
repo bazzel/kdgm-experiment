e838a193efa50e51f63a02cfb8ce2563fea0af2b
Patrick Baselier <patrick@kabisa.nl>
2015-07-07 22:07:24 +0200

    Alias browseUrl and define locateUrl

--- app/kerkdienst-gemist/model.js
+++ app/kerkdienst-gemist/model.js
@@ -1,7 +1,13 @@
+import Ember from 'ember';
 import DS from 'ember-data';
 
 export default DS.Model.extend({
   browse: DS.attr(),
   search: DS.attr(),
-  locate: DS.attr()
+  locate: DS.attr(),
+  browseUrl: Ember.computed.alias('browse'),
+  locateUrl: function() {
+    // Remove the querystring
+    return this.get('locate').split(/[?#]/)[0];
+  }.property('locate')
 });
--- app/state/adapter.js
+++ app/state/adapter.js
@@ -2,7 +2,7 @@ import ApplicationAdapter from '../application/adapter';
 
 export default ApplicationAdapter.extend({
   findAll: function() {
-    var url = this.get('kerkdienstGemist.browse');
+    var url = this.get('kerkdienstGemist.browseUrl');
     return this.ajax(url);
   }
 });
