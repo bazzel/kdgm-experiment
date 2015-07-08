287c98b8a649e770097172c92c71cfcf501a8609
Patrick Baselier <patrick@kabisa.nl>
2015-07-08 18:50:16 +0200

    Control loading behaviour

--- app/nearby/controller.js
+++ app/nearby/controller.js
@@ -3,25 +3,23 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   hasMore: function() {
     return this.meta('pagination').next;
-  }.property('meta'),
+  }.property(),
   meta: function(key) {
     return this.store.metadataFor('hit')[key];
   },
   actions: {
     loadMore: function() {
-      this.set('isLoading');
+      this.set('isLoading', true);
       var query = {
         page: this.meta('pagination').next,
         lat: this.meta('latitude'),
         lng: this.meta('longitude')
       };
 
-      console.log(this.meta('pagination').next, this.meta('pagination').page);
-      console.log(this.get('hasMore'));
-
       this.store.query('hit', query).then((data) => {
         this.get('model').pushObjects(data.get('content'));
         this.set('isLoading', false);
+        this.set('hasMore', this.meta('pagination').next);
       });
     }
   }
