d080d404cd7019945bd1a4afd77feee27cfd34cf
Patrick Baselier <patrick.baselier@gmail.com>
2015-07-14 21:52:14 +0200

    Check if in view when element inserted

--- app/components/infinite-scroll/component.js
+++ app/components/infinite-scroll/component.js
@@ -1,22 +1,24 @@
 import Ember from 'ember';
 
+// Simplified version of
+// https://github.com/hhff/ember-infinity/blob/master/addon/components/infinity-loader.js
 export default Ember.Component.extend({
-  click: function() {
-    this.sendAction();
-  },
+  //click: function() {
+    //this.sendAction();
+  //},
   _startObserveScrolling: function() {
     Ember.$(window).on('scroll', () => {
-      this._didScroll();
+      Ember.run.debounce(this, this._checkIfInView, 10);
     })
+  }.on('didInsertElement'),
+  _checkIfInView: function() {
+    if (this._isScrolledToBottom()) {
+      this.sendAction();
+    }
   }.on('didInsertElement'),
   _stopObserveScrolling: function() {
     Ember.$(window).off('scroll');
   }.on('willDestroyElement'),
-  _didScroll: function() {
-    if (this._isScrolledToBottom()) {
-      this.sendAction();
-    }
-  },
   _isScrolledToBottom: function() {
     var distanceToTop = Ember.$(document).height() - Ember.$(window).height(),
         top           = Ember.$(document).scrollTop();
