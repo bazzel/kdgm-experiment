b0d35c4fdac4e9fd7293cce5ce04ba4c7b4cbb4f
Patrick Baselier <patrick.baselier@gmail.com>
2015-07-15 12:14:12 +0200

    Load more when displaying first results

--- app/components/infinite-scroll/component.js
+++ app/components/infinite-scroll/component.js
@@ -3,15 +3,16 @@ import Ember from 'ember';
 // Simplified version of
 // https://github.com/hhff/ember-infinity/blob/master/addon/components/infinity-loader.js
 export default Ember.Component.extend({
-  //click: function() {
-    //this.sendAction();
-  //},
+  click: function() {
+    this.sendAction();
+  },
   _startObserveScrolling: function() {
     Ember.$(window).on('scroll', () => {
       Ember.run.debounce(this, this._checkIfInView, 10);
-    })
+    });
   }.on('didInsertElement'),
   _checkIfInView: function() {
+    console.log('_checkIfInView');
     if (this._isScrolledToBottom()) {
       this.sendAction();
     }
@@ -21,8 +22,14 @@ export default Ember.Component.extend({
   }.on('willDestroyElement'),
   _isScrolledToBottom: function() {
     var distanceToTop = Ember.$(document).height() - Ember.$(window).height(),
-        top           = Ember.$(document).scrollTop();
+    top           = Ember.$(document).scrollTop();
 
     return top === distanceToTop;
-  }
+  },
+  queryDidChange: Ember.observer('query', function() {
+    // Currently this ensures more than 10 items
+    // are displayed if possible.
+    // However, using a `wait` of 1.000 doesn't feel right :(
+    Ember.run.debounce(this, this._checkIfInView, 1000);
+  })
 });
